1aa675cf9a915c01fbb482f76f9aa2092bbe18b0
Guilherme Rodrigues <gadr90@gmail.com>
2018-03-15 12:26:14 -0300

    Add colossus context types

--- node/index.ts
+++ node/index.ts
@@ -1,11 +1,13 @@
-module.exports = {
+import {ColossusContext} from 'colossus'
+
+export default {
   events: {
     onAppsLinked: async (ctx) => {
       console.log(`onAppsLinked body: ${ctx.body}`)
     }
   },
   routes: {
-    printHelloWorld: async (ctx) => {
+    printHelloWorld: async (ctx: ColossusContext) => {
       ctx.set('Cache-Control', 'no-cache')
       ctx.response.status = 200
       ctx.response.body = 'Hello, IO!'
